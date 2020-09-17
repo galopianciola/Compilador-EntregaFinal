2551b883fbb5f8e9332dff9f381694c64ed062f1
galopianciola <gpianciolabartol@alumnos.exa.unicen.edu.ar>
2020-09-17 13:51:34 -0300

    Token y AccionSemantica creados

--- src/AccionSemantica1.java
+++ src/AccionSemantica1.java
@@ -1,6 +1,6 @@
 public class AccionSemantica1 {
 
-    public void run(char LETRA){
+    public void run(String LETRA){
         String aux = LETRA;
     }
 
---
+++ src/Token.java
@@ -0,0 +1,3 @@
+public class Token {
+    private int a = 5;
+}
